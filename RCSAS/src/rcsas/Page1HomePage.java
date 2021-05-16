45773e661d8765f06099c5178001d7e5e73a22f1
JingYi32 <71211315+JingYi32@users.noreply.github.com>
2021-05-16 19:36:12 +0800

    .

--- RCSAS/src/rcsas/Page1HomePage.java
+++ RCSAS/src/rcsas/Page1HomePage.java
@@ -45,6 +45,84 @@ public class Page1HomePage extends JFrame implements ActionListener{
         setVisible(true);
     }
     public void actionPerformed(ActionEvent e){
+<<<<<<< HEAD
 
+=======
+       if(e.getSource()==exit){
+            String input = JOptionPane.showInputDialog("Password:");
+            if(input.equals("12345")){
+                try{
+                    PrintWriter p = new PrintWriter("admin.txt");
+                    for(int i=0; i<RCSAS.allAdmin.size(); i++){
+                        Admin c = RCSAS.allAdmin.get(i);
+                        p.println(c.getName());
+                        p.println(c.getPin());
+                        p.println();
+                    } 
+                    p.close();                    
+                    p = new PrintWriter("booking.txt");
+                    for(int i=0; i<RCSAS.allBooking.size(); i++){
+                        Booking b = RCSAS.allBooking.get(i);
+                        p.println(b.getId());
+                        p.println(b.getHall());
+                        p.println(b.getDate());
+                        p.println(b.getCode_sp());
+                        p.println(b.getCode_sc());
+                        p.println(b.getTimeStarted());
+                        p.println(b.getDuration());
+                        p.println(b.getTimeEnded());                      
+                        p.println(b.getPrice());
+                        p.println(b.isPaid());
+                        p.println(b.getOwner().getName());
+                        p.println();               
+                    }
+                    p.close();      
+                    System.exit(0);
+                    } catch(Exception ex){
+                    System.out.println("Error in stop!");
+                }
+            } else{
+                JOptionPane.showMessageDialog(exit,"Wrong password!");
+            }
+        } else if(e.getSource()==signup){
+            String a = JOptionPane.showInputDialog("Enter name:");
+            boolean flag = true;
+            for(int i=0; i<RCSAS.allAdmin.size(); i++){
+                Admin c = RCSAS.allAdmin.get(i);
+                if(a.equals(c.getName())){
+                    flag = false;
+                    break;
+                }   
+}
+            if(flag){
+                int pin = Integer.parseInt(JOptionPane.showInputDialog("Pin"));
+                Admin c = new Admin(a,pin);
+                RCSAS.allAdmin.add(c);
+            } else{
+                JOptionPane.showMessageDialog(signup, "Name has been used!");
+            }
+        } else{
+            String s = JOptionPane.showInputDialog("Username:");
+            for(int i=0; i<RCSAS.allAdmin.size(); i++){
+                Admin c = RCSAS.allAdmin.get(i);
+                if(s.equals(c.getName())){
+                    RCSAS.whoLogin = c;
+                    break;
+                }
+            }
+            if(RCSAS.whoLogin==null){
+                JOptionPane.showMessageDialog(login, "Worng username!");
+            } else{
+                s = JOptionPane.showInputDialog("Password:");
+                if(Integer.parseInt(s) != RCSAS.whoLogin.getPin()){
+                    JOptionPane.showMessageDialog(login, "Wrong password!");
+                    RCSAS.whoLogin = null;
+                } else{
+                    setVisible(false);  //same as this.setVisible(false);
+                   // RCSAS.second.setVisible(true);
+                }
+            }
+        }
+>>>>>>> parent of b850e6d (Page2/booking)
     }
 }
